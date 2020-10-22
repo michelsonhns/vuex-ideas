42db35cd62d0f2aac9a52ee1e5dc7348ffcf3ccb
mich <michelson@poucedigital.com>
2020-10-22 21:09:38 +0200

    creation project

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Welcome to ideas.io</h1>
+    <Ideas />
+    <IdeasCreator/>
+  </div>
+</template>
+
+<script>
+import Ideas from '@/components/Ideas.vue';
+import IdeasCreator from '@/components/IdeasCreator.vue';
+export default {
+  name: 'Home',
+  components: {
+    Ideas,
+    IdeasCreator
+  },
+}
+</script>
