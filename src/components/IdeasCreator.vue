d12e8adffa6992994e3c85277363f2c63d36cf75
mich <michelson@poucedigital.com>
2020-10-22 22:14:36 +0200

    commit $store
    directement sur mutation

--- src/components/IdeasCreator.vue
+++ src/components/IdeasCreator.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
     <h2>Creator of ideas</h2>
+    <form @submit.prevent="create">
+      <label for="idea-title">Idea's title</label>
+      <input type="text" id="idea-title" v-model="title"/>
+      <button type="submit">Create</button>
+    </form>
   </div>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        title: "",
+      }
+    },
+    methods: {
+      create() {
+        const idea = {
+          createAt: new Date(),
+          title: this.title
+        };
+        this.$store.commit("CREATE_IDEAS", idea)
+        this.title = "";
+      }
+    },
   }
 </script>
 
