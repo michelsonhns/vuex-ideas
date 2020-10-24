05a4a476177ab58d939fd17bf2af9fb60a40a90b
mich <michelson@poucedigital.com>
2020-10-24 16:55:10 +0200

    add more columns

--- src/components/Ideas.vue
+++ src/components/Ideas.vue
@@ -2,11 +2,30 @@
   <div>
     <h2>My ideas</h2>
     <ul>
-       <draggable class="ideas" v-model="allIdeas">
+       <draggable class="ideas" v-model="allIdeas" group="ideas">
           <Idea v-for="idea in allIdeas" :key="idea.id" :item="idea">{{ idea.title }}</Idea>
         </draggable>
     </ul>
-   
+    <div class="destination">
+      <div>
+        <h3>Creatif</h3>
+        <draggable v-model="courses" group="ideas">
+          <Idea v-for="course in courses" :key="course.id" :item="course" />
+        </draggable>
+      </div>
+      <div>
+        <h3>Job</h3>
+        <draggable v-model="paperWork" group="ideas">
+          <Idea v-for="pw in paperWork" :key="pw.id" :item="pw" />
+        </draggable>
+      </div>
+      <div>
+        <h3>Vacation</h3>
+        <draggable v-model="creatives" group="ideas">
+          <Idea v-for="creative in creatives" :key="creative.id" :item="creative" />
+        </draggable>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,6 +48,30 @@
         },
         set(value) {
           this.$store.dispatch("saveIdeasOrdering", value)
+        }
+      },
+      courses: {
+        get() {
+          return this.$store.getters.courses;
+        },
+        set(value) {
+          this.$store.dispatch("saveCoursesOrdering", value)
+        }
+      },
+      paperWork: {
+        get() {
+          return this.$store.getters.paperWork;
+        },
+        set(value) {
+          this.$store.dispatch("SavePaperWorkOrdering", value)
+        }
+      },
+      creatives: {
+        get(){
+          return this.$store.getters.creatives;
+        },
+        set(value){
+          this.$store.dispatch("SaveCreativeOrdering", value)
         }
       }
     },
--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,9 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     ideas: {},
+    courses: [],
+    paperWork: [],
+    creatives: []
   },
   mutations: {
     CREATE_IDEAS(state, payload){
@@ -15,6 +18,15 @@ export default new Vuex.Store({
     },
     SAVE_IDEAS_ORDERING(state, payload) {
       state.ideas = { ...payload };
+    },
+    SAVE_COURSES_ORDERING(state, payload) {
+      state.courses = [ ...payload ];
+    },
+    SAVE_PAPERWORK_ORDERING(state, payload) {
+      state.paperWork = [ ...payload];
+    },
+    SAVE_CREATIVE_ORDERING(state, payload) {
+      state.creatives = [...payload];
     }
   },
   actions: {
@@ -27,6 +39,15 @@ export default new Vuex.Store({
         ideas[idea.id] = idea;
       });
       context.commit("SAVE_IDEAS_ORDERING", ideas)
+    },
+    saveCoursesOrdering(context, payload) {
+      context.commit("SAVE_COURSES_ORDERING", payload);
+    },
+    SavePaperWorkOrdering(context, payload) {
+      context.commit("SAVE_PAPERWORK_ORDERING", payload);
+    },
+    SaveCreativeOrdering(context, payload) {
+      context.commit("SAVE_CREATIVE_ORDERING", payload);
     }
   },
   getters: {
@@ -37,6 +58,15 @@ export default new Vuex.Store({
         return idea;
       });
       return allIdeas;
+    },
+    courses(state) {
+      return state.courses;
+    },
+    paperWork(state) {
+      return state.paperWork;
+    },
+    creatives(state) {
+      return state.creatives;
     }
   }
 })
