e04071702bc699ea7ca64459df4a5a8501ba4f86
mich <michelson@poucedigital.com>
2020-10-24 14:27:51 +0200

    modifier l'ordre des ideas

--- src/components/Ideas.vue
+++ src/components/Ideas.vue
@@ -2,21 +2,51 @@
   <div>
     <h2>My ideas</h2>
     <ul>
-      <li v-for="idea in allIdeas" :key="idea.id">{{ idea.title }}</li>
+       <draggable class="ideas" v-model="allIdeas">
+          <Idea v-for="idea in allIdeas" :key="idea.id" :item="idea">{{ idea.title }}</Idea>
+        </draggable>
     </ul>
+   
   </div>
 </template>
 
 <script>
+  import draggable from 'vuedraggable'
+  import Idea from '@/components/Idea.vue'
+
   export default {
+    components: {
+        draggable,
+        Idea
+    },
     computed: {
-      allIdeas() {
+      // allIdeas() {
+      //   return this.$store.getters.allIdeas; 
+      // }
+      allIdeas: {
+        get() {
         return this.$store.getters.allIdeas; 
+        },
+        set(value) {
+          this.$store.dispatch("saveIdeasOrdering", value)
+        }
       }
     },
   }
 </script>
 
 <style scoped>
+.ideas {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: center;
+  height: 30vh;
+}
 
+.destination {
+  display: flex;
+  justify-content: space-evenly;
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -12,17 +12,30 @@ export default new Vuex.Store({
       const id = Date.now();
       state.ideas[id] = payload;
       state.ideas = { ...state.ideas };
+    },
+    SAVE_IDEAS_ORDERING(state, payload) {
+      state.ideas = { ...payload };
     }
   },
   actions: {
     createIdea(context, payload) {
       context.commit("CREATE_IDEAS", payload);
+    },
+    saveIdeasOrdering(context, payload) {
+      const ideas = {} ;
+      payload.map(idea => {
+        ideas[idea.id] = idea;
+      });
+      context.commit("SAVE_IDEAS_ORDERING", ideas)
     }
   },
   getters: {
     allIdeas(state) {
-      const allIdeas = Object.keys(state.ideas).map(key => state.ideas[key]);
-      console.log(allIdeas);
+      const allIdeas = Object.keys(state.ideas).map(key => {
+        const idea = state.ideas[key];
+        idea.id = key;
+        return idea;
+      });
       return allIdeas;
     }
   }
